f0be6bba470f9539d5455268bb90371e0d78f515
hyuk22 <wjdgurdl272@gmail.com>
2018-11-24 22:57:49 +0900

    Make simple Blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,107 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    data: string,
+    timeStamp: number
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + data + timeStamp).toString();
+
+  static validateStructure = (block: Block): boolean =>
+    typeof block.index === "number" &&
+    typeof block.previousHash === "string" &&
+    typeof block.hash === "string" &&
+    typeof block.data === "string" &&
+    typeof block.timeStamp === "number";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timeStamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timeStamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timeStamp = timeStamp;
   }
 }
 
-const hyuk = new Human("hyuk", 26, "male");
+const genesisBlock: Block = new Block(0, "whatever", "", "Hello", 123456);
 
-const introduce = (person: Human): string => {
-  return `Hi, my name is ${person.name}.\nI am ${person.age} years old and ${
-    person.gender
-  }.`;
+let blockChain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockChain;
+
+const getLastBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const getHashForBlock = (block: Block): string =>
+  Block.calculateBlockHash(
+    block.index,
+    block.previousHash,
+    block.data,
+    block.timeStamp
+  );
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock = getLastBlock();
+  const newIndex = previousBlock.index + 1;
+  const previousHash = previousBlock.hash;
+  const newTimeStamp = getNewTimeStamp();
+  const newHash = Block.calculateBlockHash(
+    newIndex,
+    previousHash,
+    data,
+    newTimeStamp
+  );
+
+  const newBlock = new Block(
+    newIndex,
+    newHash,
+    previousHash,
+    data,
+    newTimeStamp
+  );
+
+  addBlock(newBlock);
+  return newBlock;
 };
 
-console.log(introduce(hyuk));
+const isBlockValid = (previousBlock: Block, candidateBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (block: Block): void => {
+  if (isBlockValid(getLastBlock(), block)) {
+    blockChain.push(block);
+  }
+};
+
+createNewBlock("junghyuk");
+createNewBlock("hyunjung");
+
+console.log(getBlockChain());
 
 export {};
